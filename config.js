ae08e2a3db4c9e993d4820978db9de21bd252411
Glitch (seemly-rune-freckle) <none>
2021-02-14 12:51:58 +0000

    🎖🐃 Checkpoint ./config.js:28471453/851

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /* Your Bot Default Prefix For Every Server */
-exports.Default_Prefix = ".";
+exports.Default_Prefix = "g.";
 /* Your Bot All Embeds Color */
 exports.Color = "RANDOM";
 /* Your Bot Token (Secret) */
-exports.Token = "ODEwNDI2MDIyMzIzMjkwMTIy.YCjeDA.xx0U_-TAMlLlyA-RTDLQIv_l0xM";
+exports.Token = "NzU4MzEyMzQyOTI4MDk3MzMw.X2tHZw.-i3oq82tO2Njy4L5rHdT7Ek1z1E";
 /* Bot's Owner ID */
-exports.OwnerID = "810426022323290122";
+exports.OwnerID = "758312342928097330";
